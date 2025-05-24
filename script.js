3ebdb287e62710d8ec331b93ed0736204b05439b
Sujen Rajthala <sujenrajthala122@gmail.com>
2025-05-24 11:45:44 +0545

    Adding Geolocation

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      // console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI1MDUyMS4wIKXMDSoASAFQAw%3D%3D`
+      );
+    },
+    function () {
+      alert(`Couldn't get your Position`);
+    }
+  );
