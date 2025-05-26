9e176da862df618d570d732ebad6d0f21675fc18
Sujen Rajthala <sujenrajthala122@gmail.com>
2025-05-26 19:51:59 +0545

    Displaying Map

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//http://localhost:5500
+
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -16,10 +18,23 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+      const coords = [latitude, longitude];
       // console.log(latitude, longitude);
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI1MDUyMS4wIKXMDSoASAFQAw%3D%3D`
       );
+      const map = L.map('map').setView(coords, 13);
+
+      // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert(`Couldn't get your Position`);
